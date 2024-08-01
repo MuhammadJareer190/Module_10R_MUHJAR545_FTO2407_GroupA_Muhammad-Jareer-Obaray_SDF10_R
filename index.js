1c4f4f9c4f992f1fe30e24fcaa3a9ad1fc5ff600
Muhammad Jareer <jareerobaray@icloud.com>
2024-08-01 20:00:07 +0200

    Made some changes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
+import { initializeApp } from "https://playground-b6b46-default-rtdb.europe-west1.firebasedatabase.app/"
+import { getDatabase } from ""
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
